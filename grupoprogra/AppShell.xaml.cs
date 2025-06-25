5b484500093f3dbf370f3b046f47889bf5f1de03
QuispeRonny <ronny.quispe@udla.edu.ec>
2025-06-25 18:37:20 -0500

    Agregar archivos de proyecto.

---
+++ grupoprogra/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace grupoprogra
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ grupoprogra/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace grupoprogra
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
